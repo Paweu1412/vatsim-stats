04bdacae0fd0995faa28d41b23f5941b0d192469
Paweł Nosalski <fordcrown2009@gmail.com>
2024-04-11 23:13:05 +0200

    beginning of first module

---
+++ index.js
@@ -0,0 +1,29 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { initRoleAssignmentsModule } from './modules/rolesAssignments.js';
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_CHANNEL_ID = process.env.BOT_CHANNEL_ID;
+const BOT_GUILD_ID = process.env.BOT_GUILD_ID;
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers]
+});
+
+client.once("ready", async () => {
+  const guild = client.guilds.cache.get(BOT_GUILD_ID);
+
+  if (!guild) {
+    console.error("Guild not found");
+    return;
+  }
+
+  await guild.members.fetch();
+
+  initRoleAssignmentsModule(guild);
+});
+
+
+client.login(BOT_TOKEN);
