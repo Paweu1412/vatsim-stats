a5fc014ac28d33c397a039428d49b4e4af3ee73a
Paweł Nosalski <fordcrown2009@gmail.com>
2024-04-23 14:37:21 +0200

    fix

--- prototypes.js
+++ prototypes.js
@@ -24,49 +24,60 @@ GuildMember.prototype.getNetworkPilotTime = async function () {
 
   const networkCID = this.getNetworkCID();
 
-  const response = await fetch(`https://api.vatsim.net/v2/members/${networkCID}/stats`);
-  if (!response) { return null; }
+  try {
+    const response = await fetch(`https://api.vatsim.net/v2/members/${networkCID}/stats`);
 
-  const data = await response.json();
-  if (!data?.pilot) { return null; }
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
 
-  if (!additionalTime[networkCID]) {
-    additionalTime[networkCID] = {};
-  }
+    const data = await response.json();
+    if (!data?.pilot) { return null; }
 
-  const responseSecond = await fetch(`https://api.vatsim.net/v2/members/${networkCID}/history`);
-  if (!responseSecond) { return null; }
+    if (!additionalTime[networkCID]) {
+      additionalTime[networkCID] = {};
+    }
 
-  const dataSecond = await responseSecond.json();
-  if (!dataSecond) { return null; }
+    const responseSecond = await fetch(`https://api.vatsim.net/v2/members/${networkCID}/history`);
 
-  for (const item of dataSecond.items) {
-    const memberCID = item.vatsim_id;
-    const memberCallsign = item.callsign;
+    if (!responseSecond.ok) {
+      throw new Error(`Network response was not ok: ${responseSecond.statusText}`);
+    }
 
-    const startTime = new Date(item.start);
-    const endTime = item.end === null ? new Date() : new Date(item.end);
+    const dataSecond = await responseSecond.json();
+    if (!dataSecond) { return null; }
 
-    const today = new Date();
-    if (endTime.toDateString() !== today.toDateString()) { continue; }
+    for (const item of dataSecond.items) {
+      const memberCID = item.vatsim_id;
+      const memberCallsign = item.callsign;
 
-    const timeDifference = endTime - startTime;
-    const hoursDifference = ((timeDifference / 1000) / 3600).toFixed(2);
+      const startTime = new Date(item.start);
+      const endTime = item.end === null ? new Date() : new Date(item.end);
 
-    if (additionalTime[memberCID]) {
-      if (!additionalTime[memberCID][memberCallsign]) {
-        additionalTime[memberCID][memberCallsign] = hoursDifference;
-      }
+      const today = new Date();
+      if (endTime.toDateString() !== today.toDateString()) { continue; }
 
-      if (additionalTime[memberCID][memberCallsign] !== hoursDifference) {
-        additionalTime[memberCID][memberCallsign] = hoursDifference;
+      const timeDifference = endTime - startTime;
+      const hoursDifference = ((timeDifference / 1000) / 3600).toFixed(2);
+
+      if (additionalTime[memberCID]) {
+        if (!additionalTime[memberCID][memberCallsign]) {
+          additionalTime[memberCID][memberCallsign] = hoursDifference;
+        }
+
+        if (additionalTime[memberCID][memberCallsign] !== hoursDifference) {
+          additionalTime[memberCID][memberCallsign] = hoursDifference;
+        }
       }
     }
+
+    console.log(`[ROLES] Fetched ${this.user.username} hours on VATSIM / ${data.pilot} + ${getSumOfAdditionalTime(networkCID)} (${data.pilot + getSumOfAdditionalTime(networkCID)})`);
+
+    return data.pilot + getSumOfAdditionalTime(networkCID);
+  } catch (error) {
+    console.error('Error during network request:', error);
+    return null;
   }
-
-  console.log(`[ROLES] Fetched ${this.user.username} hours on VATSIM / ${data.pilot} + ${getSumOfAdditionalTime(networkCID)} (${data.pilot + getSumOfAdditionalTime(networkCID)})`);
-
-  return data.pilot + getSumOfAdditionalTime(networkCID);
 }
 
 String.prototype.format = function () {
